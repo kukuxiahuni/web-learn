6b91d13a7b02e44a834df135b18120d00e30b494
huangchuancong <kukuxiahuni@gmail.com>
2017-11-30 21:33:01 +0800

    1. fix init

---
+++ src/main/java/me/learn/web/architecture/HelloServlet.java
@@ -0,0 +1,23 @@
+package me.learn.web.architecture;
+
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * web
+ * me.learn.web.architecture
+ * Created by kukuxiahuni on 2017/11/30.
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
